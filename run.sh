8d2ee32c743de895c620dc246349d0e95659924b
SatoshiTerasaki <terasakisatoshi.math@gmail.com>
2024-10-03 20:30:46 +0900

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,1 +1,9 @@
-julia +nightly --project juliac.jl --output-exe main buildmodule.jl && ./main
+julia +nightly --version
+
+echo Compile Julia package TerminalSystemMonitor.jl using juliac.jl
+
+julia +nightly --project juliac.jl --output-exe main buildmodule.jl
+
+echo Compilation done!
+
+./main
